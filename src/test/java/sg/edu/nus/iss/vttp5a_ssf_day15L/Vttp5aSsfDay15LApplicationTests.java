0a537425dcb1561499c74ce3856e76c96ee07aa1
maziyyahjaf <mazi3ah@gmail.com>
2024-11-22 11:01:01 +0800

    first

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_ssf_day15L/Vttp5aSsfDay15LApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day15L;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay15LApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
